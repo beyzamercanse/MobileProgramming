fa276dbb09e17813c156c87e783851ff405622ec
beyzamercan97 <beyzamercan@yonsei.ac.kr>
2021-09-14 21:24:09 +0300

    수정 사항 commit

--- app/src/main/java/com/example/programmingmobile/MainActivity.java
+++ app/src/main/java/com/example/programmingmobile/MainActivity.java
@@ -6,14 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.programmingmobile.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 
     public void onClick(View view) {
+        binding.imageView.setImageResource(R.drawable.city);
         Toast.makeText(this, "안녕하세요 버튼 클릭입니다!", Toast.LENGTH_LONG).show();
     }
 }
